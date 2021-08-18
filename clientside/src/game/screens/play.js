ae85df423918c80f0a8501ec0654f7568e99d2a5
theweinzierl <57496177+theweinzierl@users.noreply.github.com>
2021-08-18 13:33:31 +0200

    Update play.js

--- clientside/src/game/screens/play.js
+++ clientside/src/game/screens/play.js
@@ -6,15 +6,8 @@ game.PlayScreen = me.Stage.extend({
 
         me.levelDirector.loadLevel("desert");
 
-        if(game.mode === "multiplayer" && game.netCom !== null){                     
-            game.retep = me.game.world.addChild(me.pool.pull("Retep", 32, 544, game.netCom.getOpponentName()));
-            game.netCom.onGameDataReceived = game.onGameDataReceived.bind(game);
-        }
-
     },
 
     onDestroyEvent: function() {
-        // remove the HUD from the game world
-        me.game.world.removeChild(this.HUD);
     }
 });
