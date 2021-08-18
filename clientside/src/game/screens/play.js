c876294e29884c0281084b00bf4184f4da8af776
theweinzierl <57496177+theweinzierl@users.noreply.github.com>
2021-08-18 14:25:21 +0200

    Update play.js

--- clientside/src/game/screens/play.js
+++ clientside/src/game/screens/play.js
@@ -2,15 +2,13 @@
 game.PlayScreen = me.Stage.extend({
 
     onResetEvent: function() {
-        // load a level
-        me.game.onLevelLoaded = () => {
-            console.log("here i am!");
-            if(game.mode === "multiplayer" && game.netCom !== null){                     
-                game.retep = me.game.world.addChild(me.pool.pull("Retep", 32, 544, game.netCom.getOpponentName()));
-                game.netCom.onGameDataReceived = game.onGameDataReceived.bind(game);
-            }
-        };
+
         me.levelDirector.loadLevel("desert");
+        
+        if(game.mode === "multiplayer" && game.netCom !== null){                     
+            game.retep = me.game.world.addChild(me.pool.pull("Retep", 32, 544, game.netCom.getOpponentName()));
+            game.netCom.onGameDataReceived = game.onGameDataReceived.bind(game);
+        }
 
     },
 
